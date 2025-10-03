60c1eac8e3ff9bdb8ba099250b692ff275a5271c
Alexander Sergeev <aleksandr.sergeev@izex.ru>
2025-10-03 17:40:27 +0300

    init

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'myApp',
+  appId: 'com.aplgo.ionicapp',
+  appName: 'APL Mobile',
   webDir: 'dist'
 };
 
